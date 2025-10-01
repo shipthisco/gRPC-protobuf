c2a5514b870f80085e03a376fc67c6c28da0502c
mayurrawte <mayur@shipthis.co>
2025-10-01 16:02:04 +0530

    Updated actions:

--- scripts/python/generate_lib.py
+++ scripts/python/generate_lib.py
@@ -97,7 +97,7 @@ setup(
     install_requires=[
         'grpcio>=1.48.0',
         'grpcio-tools>=1.48.0',
-        'protobuf>=4.25.0',
+        'protobuf>=4.21.0,<6.0.0',  # Support older and newer protobuf versions
     ],
     python_requires='>=3.8',
     keywords=['python', 'gRPC', 'protobuf', 'pyshipproto'],
